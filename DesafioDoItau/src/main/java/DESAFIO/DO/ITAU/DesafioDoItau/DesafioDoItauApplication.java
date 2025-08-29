c40881c2f3e2190503d25c3faa886945f26f4c27
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-08-29 10:10:15 -0300

    primeito commit adicionando ambiente spring

---
+++ DesafioDoItau/src/main/java/DESAFIO/DO/ITAU/DesafioDoItau/DesafioDoItauApplication.java
@@ -0,0 +1,13 @@
+package DESAFIO.DO.ITAU.DesafioDoItau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDoItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDoItauApplication.class, args);
+	}
+
+}
