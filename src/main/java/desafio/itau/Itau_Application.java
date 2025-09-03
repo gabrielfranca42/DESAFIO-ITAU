b8761c4a4c6482f992b7226ed196d2b3ae605ffb
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-09-03 13:49:20 -0300

    teste de corno

--- src/main/java/desafio/itau/Itau_Application.java
+++ src/main/java/desafio/itau/Itau_Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Itau_Application {
 
 	public static void main(String[] args) {
-
+    System.out.println("teste se essa porra esta comitando");
 		SpringApplication.run(Itau_Application.class, args);
 
 
